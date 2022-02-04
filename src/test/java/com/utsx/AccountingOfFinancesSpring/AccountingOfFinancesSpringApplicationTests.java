2f30afeafc597f0afc31587ddcb875540a424a8e
Дмитрий <dimma1506@icloud.com>
2022-02-04 21:47:03 +0300

    Configure Spring
    Configuration Spring. Usage  Spring Boot, Spring Security, Spring Data JPA, Thymeleaf.

---
+++ src/test/java/com/utsx/AccountingOfFinancesSpring/AccountingOfFinancesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utsx.AccountingOfFinancesSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingOfFinancesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
